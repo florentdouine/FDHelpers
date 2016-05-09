5ca01b4e7dcf1ce2fa30934cc15578327c08162e
Flodou <florent@douine.fr>
2016-05-09 18:15:58 +0200

    New file

---
+++ Classes/FDHelpers.swift
@@ -0,0 +1,17 @@
+//
+//  FDHelpers.swift
+//  Neko
+//
+//  Created by Florent Douine on 09/05/2016.
+//  Copyright © 2016 Florent Douine. All rights reserved.
+//
+
+import Foundation
+
+class FDHelpers {
+    
+    init(){
+        
+        let t = FDField();
+    }
+}
